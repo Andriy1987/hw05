40657cf6e5c61c9e52f403f9143ce8f2873d0e99
Andriy <alavrinok@gmail.com>
2018-03-30 15:41:12 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -3,14 +3,14 @@ const b = parseFloat(prompt ('type b'));
 const c = parseFloat(prompt ('type c'));
 
 const result = solveQuad(a, b, c);
-document.write (result);
+
 
 function solveQuad (a, b, c) {
     const d = calcDisc(a, b, c);
     if (d>0){
        x1 = (-b + Math.sqrt(d))/2*a;
        x2 = (-b - Math.sqrt(d))/2*a;
-    return 'x1= ' + x1  + 'x2 = ' + x2; 
+    document.write('x1=' + x1 +'<br>' + 'x2='+ x2)
     }
         else if (d===0){ 
             x1 = -(b/2*a);
@@ -21,6 +21,3 @@ function calcDisc(a, b, c) {
     return b**2 - 4*a*c;
 }
 
-function prn (val) {
-    document.write (val + '<br');
-}
